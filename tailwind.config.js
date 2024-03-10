3b3623747629c39380505f2febd70e79b7b592a0
hamza <hamza786504@gmail.com>
2024-03-10 13:16:32 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
+    extend: {
+      colors: {
+        primary: "#612fff",
+      },
+    },
+  },
+  plugins: [],
+}
